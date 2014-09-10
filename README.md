ca6c336b1b47006d49b13124b6c307444a42f18a
Adam Wentz <awentz@theonion.com>
2014-09-09 22:05:56 -0500

    Added example with matplotlab and gipc

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Installation
  * `python setup.py install`
 
 
+Examples
+--------
+ * lolstream.py: print out a live stream of tweets containing lol
+ * plottin.py: create matplotlib images using gipc to create gevent-friendly child processes
+
+
 Develop
 -------
  * Clone
--- examples/plottin.py
+++ examples/plottin.py
@@ -4,15 +4,18 @@ from gevent.monkey import patch_all; patch_all()
 import time
 
 import gevent
+import gipc
 import matplotlib.pyplot as plt
 from gevent.queue import Queue
 from gtwittools.gutils import echo_queue, spawn_worker
 
 
-def sampler(buffer_q, fn, interval=0.01, flush_size=100):
+# sampler process
+
+def sampler(buffer_q, gen, interval=0.01, flush_size=100):
     buffer = []
     while True:
-        value = fn()
+        value = next(gen)
         buffer.append(value)
         if len(buffer) == flush_size:
             buffer_q.put(buffer)
@@ -20,22 +23,34 @@ def sampler(buffer_q, fn, interval=0.01, flush_size=100):
         gevent.sleep(interval)
 
 
-def plotter(conf_q, rendered_q):
+def sampler_process(buffer_writer, fn):
+    spawn_worker([
+        (sampler, buffer_writer, fn),
+        #(echo_queue, buffer_q),
+    ])
+
+
+# renderer process
+
+def plotter(conf_q, rendered_q, output_dir='/tmp/'):
     import os
     import shutil
     import tempfile
 
-    temp_dir = '/tmp/' # tempfile.mkdtemp()
+    if not output_dir:
+        output_dir = tempfile.mkdtemp()
     num_plots = 0
     for plot_conf in conf_q:
-        filename = os.path.join(temp_dir, '{}.png'.format(num_plots))
+        filename = os.path.join(output_dir, '{}.png'.format(num_plots))
         render_plot(plot_conf, filename)
         num_plots += 1
         rendered_q.put(filename)
-    shutil.rmtree(temp_dir, ignore_errors=True)  # clean up those files!
+    shutil.rmtree(output_dir, ignore_errors=True)  # clean up those files!
 
 
 def render_plot(plot_conf, filename='/tmp/test.png'):
+    # TODO: learn matlabplot
+    plt.clf()
     values = plot_conf.pop('values')
     plt.plot(values)
     for key, value in plot_conf:
@@ -43,46 +58,64 @@ def render_plot(plot_conf, filename='/tmp/test.png'):
     plt.savefig(filename)
 
 
-def sample_plotter(buffer_q, plot_q, rendered_q):
-    for values in buffer_q:
+def configure_plots(buffer_reader, plot_q):
+    """Transform the sample buffer into some plotting commands."""
+    while True:
+        values = buffer_reader.get()
         conf = dict(
             values=values
         )
         plot_q.put(conf)
 
 
-class TWrap(object):
-    """Wrap a function with a timer"""
-    def __init__(self, f, scale=1.0, offset=0.0):
-        self.f = f
-        self.t = offset
-        self.last_t = None
-        self.scale = scale
+def renderer_process(buffer_reader, output_dir='/tmp/'):
+    plot_q = Queue()
+    rendered_q = Queue()
+    spawn_worker([
+        (configure_plots, buffer_reader, plot_q),
+        (plotter, plot_q, rendered_q, output_dir),
+        (echo_queue, rendered_q),
+    ])
 
-    def __call__(self):
-        if self.last_t is None:
-            self.last_t = time.time()
+
+# utils
+
+def gen_t(f, scale=1.0, initial_t=0.0):
+    t = initial_t
+    last_t = None
+    while True:
+        if last_t is None:
+            last_t = time.time()
         now = time.time()
-        self.t += self.scale * (self.last_t - now)
-        self.last_t = now
-        return self.f(self.t)
+        t += scale * (last_t - now)
+        last_t = now
+        yield f(t)
+
+
+def spawn_processes(confs):
+    processes = []
+    for conf in confs:
+        p = gipc.start_process(target=conf[0], args=tuple(conf[1:]))
+        processes.append(p)
+    return processes
 
 
 def main():
     import math
+    sin = gen_t(math.sin, scale=5.0)
 
-    buffer_q = Queue()
-    plot_q = Queue()
-    rendered_q = Queue()
-    sin = TWrap(math.sin, scale=5.0)
-    conf = [
-        (sampler, buffer_q, sin),
-        (sample_plotter, buffer_q, plot_q, rendered_q),
-        (plotter, plot_q, rendered_q),
-        (echo_queue, rendered_q),
-    ]
-    spawn_worker(conf)
+    buffer_reader, buffer_writer = gipc.pipe()
+    processes = spawn_processes([
+        (sampler_process, buffer_writer, sin),
+        (renderer_process, buffer_reader,),
+    ])
+    while True:
+        gevent.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+
